8b697f8603519d480189f0df16f17bd0f60b4618
dtris <daohuutri04@gmail.com>
2025-02-22 13:52:02 +0700

    add new user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,19 +1,33 @@
+const { render, name } = require("ejs");
 const conection = require("../config/database");
 
 const getHomepage = (req, res) => {
-  let users = [];
-  conection.query("select * from Users", function (err, result, fields) {
-    users = result;
-    console.log("check result>>>", result);
-    console.log("check user>>>: ", users);
-    res.send(JSON.stringify(users));
-  });
-  //xu ly data
+  return res.render("home.ejs");
 };
+//xu ly data
 const getABC = (req, res) => {
   res.render("sample.ejs");
 };
+const postCreateUser = (req, res) => {
+  let email = req.body.email;
+  let name = req.body.myName;
+  let city = req.body.city;
+  console.log("email = ", email, "name = ", name, "city = ", city);
+  // let { email, myName, city } = req.body;
+  //   INSERT INTO Users(email, name, city)
+  // VALUES('dtris1412@gmail.com', 'le thi v', 'anam dinh')
+  conection.query(
+    `INSERT INTO Users(email, name, city) VALUES(?,?,?)`,
+    [email, name, city],
+    function (err, result) {
+      console.log(result);
+      res.send("create user successed");
+    }
+  );
+};
+
 module.exports = {
   getHomepage,
   getABC,
+  postCreateUser,
 };
--- src/server.js
+++ src/server.js
@@ -5,9 +5,16 @@ const app = express();
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
 const conection = require("./config/database");
+const exp = require("constants");
 const port = process.env.PORT;
 const hostname = process.env.HOST_NAME;
-
+//config request.body
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 configViewEngine(app);
 app.use("/", webRoutes);
 conection.query("select * from Users", function (err, result, fields) {
